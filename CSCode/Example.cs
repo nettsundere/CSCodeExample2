7c075f9ab3aec802a1421c53324d1c902fb00def
Vladimir nettsundere Kiselev <nettsundere@gmail.com>
2017-08-09 06:59:48 +0300

    Add material

---
+++ CSCode/Example.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSCode
+{
+    interface IAreaCalculable<T>
+    {
+        T GetArea();
+    }
+
+    /// <summary>
+    /// The circle.
+    /// </summary>
+    class Circle : IAreaCalculable<double>
+    {
+        public double Raduisss { get; set; }
+
+        public double GetArea()
+        {
+            Console.WriteLine("Getting an area of a circle...");
+
+            return Math.PI * Raduisss * Raduisss;
+        }
+    }
+
+    /// <summary>
+    /// The triangle.
+    /// </summary>
+    class Triangle : IAreaCalculable<double>
+    {
+        public double Height { get; set; }
+        public double Base { get; set; }
+
+        public virtual double GetArea()
+        {
+            Console.WriteLine("Getting an area of a triangle...");
+
+            return 0.5 * Height * Base;
+        }
+    }
+
+    /// <summary>
+    /// Triangle, but with a hole inside.
+    /// </summary>
+    class TriangleWithHole : Triangle
+    {
+        public Circle Hole { get; set; }
+
+        public override double GetArea()
+        {
+            Console.WriteLine("Getting an area of a triangle minus an area of a circle...");
+
+            return base.GetArea() - Hole.GetArea();
+        }
+    }
+}
---
+++ CSCode/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var triangle = new TriangleWithHole()
+            {
+                Height = 1000,
+                Base = 100,
+                Hole = new Circle()
+                {
+                    Raduisss = 10
+                }
+            };
+
+            triangle.GetArea();
+
+            Console.ReadKey();
+        }
+    }
+}
